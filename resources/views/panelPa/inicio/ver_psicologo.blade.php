105c97c8805a1e21d721a7835bac9805b6f8fb3c
Alonsou29 <alonsourdaneta.29@hotmail.com>
2024-07-11 23:30:08 -0400

    vistas de psicologo 0.1

--- resources/views/panelPa/inicio/ver_psicologo.blade.php
+++ resources/views/panelPa/inicio/ver_psicologo.blade.php
@@ -1,6 +1,22 @@
 @extends('adminlte::page')
 @vite('resources/css/app.css')
 
+@php
+    use App\Models\psicologo_paciente;
+    use App\Models\Psicologos;
+    use App\Models\User;
+    use App\Models\Paciente;
+
+    $paciente = Paciente::where('id_user', auth()->user()->id)->first();
+
+    $busqueda = psicologo_paciente::where('paciente_id', $paciente->id)->first();
+
+    $buscarpsicologo = Psicologos::find($busqueda->psicologo_id)->first();
+
+    $UserPsico = User::where('id',$buscarpsicologo->id_user)->first();
+
+    // dd($UserPsico, $UserPsico);
+@endphp
 
 @section('content')
 
@@ -9,7 +25,7 @@
   <div class="bg-gray-100 lg:mr-5 ">
     <div class="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5">
       <img class="w-32 h-32 rounded-full mx-auto" src="https://picsum.photos/200" alt="Profile picture">
-      <h2 class="text-center text-2xl font-semibold mt-3">John Doe</h2>
+      <h2 class="text-center text-2xl font-semibold mt-3">{{$UserPsico->first_name}} {{$UserPsico->last_name}}</h2>
       <div class="mt-5">
         <h3 class="text-xl font-semibold">Biografía</h3>
         <p class="text-gray-600 mt-2">John is a software engineer with over 10 years of experience in developing web and mobile applications. He is skilled in JavaScript, React, and Node.js.</p>
@@ -20,26 +36,26 @@
   <div class="bg-white shadow-md rounded-lg lg:w-[700px] max-w-[95%] mx-auto mt-5 p-3 lg:p-5">
     <div class=" mb-8 w-full ">
       <h4 class="px-2 text-xl font-bold text-navy-700 text-black">Información General</h4>
-    </div> 
+    </div>
     <div class="grid grid-cols-2 gap-4 px-2">
       <div class="flex flex-col items-start justify-center rounded-2xl bg-contenidocarta bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
         <p class="text-sm text-gray-600">Telefono</p>
-        <p class="text-base font-medium text-navy-700 dark:text-white">0412-6664851</p>
+        <p class="text-base font-medium text-navy-700 dark:text-white">{{$UserPsico->telefono}}</p>
       </div>
 
       <div class="flex flex-col justify-center rounded-2xl bg-contenidocarta bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
         <p class="text-sm text-gray-600">Email</p>
-        <p class="text-base font-medium text-navy-700 dark:text-white">jonhdoe@gmail.com</p>
+        <p class="text-base font-medium text-navy-700 dark:text-white">{{$UserPsico->email}}</p>
       </div>
 
       <div class="flex flex-col items-start justify-center rounded-2xl bg-contenidocarta bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
         <p class="text-sm text-gray-600">Fecha Nacimiento</p>
-        <p class="text-base font-medium text-navy-700 dark:text-white">20/10/1999</p>
+        <p class="text-base font-medium text-navy-700 dark:text-white">{{$UserPsico->birthdate}}</p>
       </div>
 
       <div class="flex flex-col justify-center rounded-2xl bg-contenidocarta bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
         <p class="text-sm text-gray-600">Descripción del Problema</p>
-        <p class="text-base font-medium text-navy-700 dark:text-white">xd</p>
+        <p class="text-base font-medium text-navy-700 dark:text-white">{{$buscarpsicologo->Descripcion}}</p>
       </div>
     </div>
   </div>
